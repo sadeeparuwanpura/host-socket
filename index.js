d40c4d2e3b5dc6c8ab80bc9e483edff92c276a2f
sadeeparuwanpura <sadeepa.ruwanpura@layoutindex.lk>
2024-06-13 16:18:25 +0530

    updaate

--- index.js
+++ index.js
@@ -1,20 +1,60 @@
+// const express = require('express');
+// const http = require('http');
+// const socketIO = require('socket.io-client'); // Import socket.io-client
+
+// const app = express();
+// const server = http.createServer(app);
+
+// const io = require('socket.io')(server, {
+//     cors: {
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST'],
+//         allowedHeaders: ['Content-Type'],
+//         credentials: true,
+//     },
+// });
+
+// const localServerSocket = socketIO('http://192.168.21.232:4005');
+
+// io.on('connection', (socket) => {
+//   console.log('Client connected');
+
+//   socket.on('print', (data) => {
+//     console.log('Print request received:', data);
+//     try {
+//         //localServerSocket.emit('print', data); 
+//         localServerSocket.emit('print', data); 
+
+//     } catch (error) {
+//         console.log("Error Occur: "+error);
+//     } 
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log('Client disconnected');
+//   });
+// });
+
+// const port = process.env.PORT || 3001;
+// server.listen(port, () => {
+//   console.log(`Hosted Node app listening on port ${port}`);
+// });
+
+
 const express = require('express');
 const http = require('http');
-const socketIO = require('socket.io-client'); // Import socket.io-client
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-
-const io = require('socket.io')(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST'],
-        allowedHeaders: ['Content-Type'],
-        credentials: true,
-    },
+const io = socketIO(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+  },
 });
-
-//const localServerSocket = socketIO('http://192.168.21.232:4005');
 
 io.on('connection', (socket) => {
   console.log('Client connected');
@@ -22,12 +62,12 @@ io.on('connection', (socket) => {
   socket.on('print', (data) => {
     console.log('Print request received:', data);
     try {
-        //localServerSocket.emit('print', data); 
-        socket.emit('print', data); 
-
+      const electronAppSocket = socketIO.connect('http://192.168.21.232:8000');
+      electronAppSocket.emit('print', data);
+      electronAppSocket.disconnect();
     } catch (error) {
-        console.log("Error Occur: "+error);
-    } 
+      console.log('Error:', error);
+    }
   });
 
   socket.on('disconnect', () => {
@@ -37,5 +77,5 @@ io.on('connection', (socket) => {
 
 const port = process.env.PORT || 3001;
 server.listen(port, () => {
-  console.log(`Hosted Node app listening on port ${port}`);
+  console.log(`Hosted Node.js server listening on port ${port}`);
 });
